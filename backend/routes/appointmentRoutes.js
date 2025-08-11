ab4ea1ff6f507bcc8051b9f4f0853c4a32d92928
Ubuntu <ubuntu@ip-172-31-98-112.ap-southeast-2.compute.internal>
2025-08-11 17:26:21 +0000

    fix appointmentRoutes to run pm2

--- backend/routes/appointmentRoutes.js
+++ backend/routes/appointmentRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const appointmentController = require('../controllers/appointmentController');
 const authMiddleware = require('../middleware/authMiddleware');
 
-router.get('/', authMiddleware, appointmentController.getAppointments);
-router.post('/', authMiddleware, appointmentController.addAppointment);
-router.put('/:id', authMiddleware, appointmentController.updateAppointment);
-router.delete('/:id', authMiddleware, appointmentController.deleteAppointment);
+router.get('/', authMiddleware.protect, appointmentController.getAppointments);
+router.post('/', authMiddleware.protect, appointmentController.addAppointment);
+router.put('/:id', authMiddleware.protect, appointmentController.updateAppointment);
+router.delete('/:id', authMiddleware.protect, appointmentController.deleteAppointment);
 
 module.exports = router;
--- backend/routes/petRoutes.js
+++ backend/routes/petRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const petController = require('../controller/petController');
+const petController = require('../controllers/petController');
 
 router.get('/', petController.getPets);
 router.post('/', petController.addPet);
